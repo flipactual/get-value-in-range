48716be7eb526f862584901c6ccd35bf831f6d7d
Flip Actual <flipactual@gmail.com>
2016-11-30 20:41:01 -0600

    switch to ava

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
-const assert = require('assert');
+const test = require('ava');
 const getValueInRange = require('../src');
 
-describe('getValueInRange', () => {
-  it('Returns 0 for the minimum value in the range', () => {
-    assert.equal(getValueInRange(5, [10, 5]), 0);
-  });
-  it('Returns 0.5 for the middle value in the range', () => {
-    assert.equal(getValueInRange(7.5, [10, 5]), 0.5);
-  });
-  it('Returns 1 for the maximum value in the range', () => {
-    assert.equal(getValueInRange(10, [10, 5]), 1);
-  });
+test('Returns 0 for the minimum value in the range', t => {
+  t.is(getValueInRange(5, [10, 5]), 0);
+});
+
+test('Returns 0.5 for the middle value in the range', t => {
+  t.is(getValueInRange(7.5, [10, 5]), 0.5);
+});
+
+test('Returns 1 for the maximum value in the range', t => {
+  t.is(getValueInRange(10, [10, 5]), 1);
 });
